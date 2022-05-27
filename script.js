4df7c8b3572dd3f1f33322de78bbb2368cbde661
Spelljinxer <spelljinxer@DESKTOP-5LQ8J1I.localdomain>
2022-05-27 22:27:55 +0800

    more about stuff

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ const faders = document.querySelectorAll(".fade-in");
 
 const appearOptions = {
     threshold: 1,
-    rootMargin: "0px 0px -100px 0px"
+    rootMargin: "0px 0px -50px 0px"
 };
 
-const appearOnScroll = new IntersectionObserver(function(
-    entries, appearOnScroll
-) {
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) 
+{
     entries.forEach(entry =>{
         if(!entry.isIntersecting){
             return;
@@ -25,3 +24,22 @@ appearOptions);
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 })
+//---------------------------------------------------------------------------------
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 300;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
