88b27fa0c57f76555080d644aa74406d13a9256c
Spelljinxer <spelljinxer@DESKTOP-5LQ8J1I.localdomain>
2022-06-21 00:59:11 +0800

    animationsgit commit -m!

--- script.js
+++ script.js
@@ -218,3 +218,43 @@ function tick() {
 }
 
 tick();
+
+const typedSpan = document.getElementById("typed")
+const totype = ["Programmer.", "Graphic Designer.", "Anime Enthusiast.", "Gacha Gambler."]
+
+const delayTyping_char = 95;
+const delayErasing_text = 150;
+const delayTyping_text = 800;
+
+let totypeIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+	if (charIndex < totype[totypeIndex].length) {
+		typedSpan.textContent += totype[totypeIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(typeText, delayTyping_char);
+	}
+	else {
+		setTimeout(eraseText, delayTyping_text);
+	}
+}
+
+function eraseText() {
+	if (charIndex > 0) {
+		typedSpan.textContent = totype[totypeIndex].substring(0, charIndex-1);
+		charIndex = charIndex-1;
+		setTimeout(eraseText, delayErasing_text);
+	}
+	else {
+		totypeIndex++;
+
+		if (totypeIndex >= totype.length)
+			totypeIndex = 0;
+			setTimeout(typeText, delayTyping_text);
+	}
+}
+
+window.onload = function() {
+	if (totype[totypeIndex].length) setTimeout(typeText, delayTyping_text);
+}
