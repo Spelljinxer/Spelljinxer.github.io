8efb7098e5cb8823f9ca4b109f57a10bab130ba1
Spelljinxer <reiden556@gmail.com>
2023-06-07 20:08:21 +0800

    add more stars

--- newsite/home.js
+++ newsite/home.js
@@ -1,31 +1,24 @@
 // Function to generate random number within a range
 function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-  }
+  return Math.random() * (max - min) + min;
+}
 
-  // Create a specified number of stars with parallax effect
-  function createStars() {
-    var stars = document.getElementById("stars");
+// Create a specified number of stars with parallax effect
+function createStars() {
+  var starsContainer = document.getElementById("stars");
 
-    for (var i = 0; i < 500; i++) {
-      var star = document.createElement("div");
-      star.className = "star";
-      star.style.top = getRandomNumber(0, 100) + "%";
-      star.style.left = getRandomNumber(0, 100) + "%";
-      star.style.animationDelay = getRandomNumber(0, 10) + "s";
-      star.style.animationDuration = getRandomNumber(5, 10) + "s";
-      star.style.setProperty('--parallax-depth', getRandomNumber(1, 5));
-      stars.appendChild(star);
-    }
-  }
-
-  createStars();
-
-  // Randomly move stars within the container
-  var stars = document.getElementsByClassName("star");
-  for (var i = 0; i < stars.length; i++) {
-    var star = stars[i];
-    star.style.animationDelay = getRandomNumber(0, 10) + "s";
+  for (var i = 0; i < 1000; i++) {
+    var star = document.createElement("div");
+    star.className = "star";
+    star.style.top = getRandomNumber(0, 100) + "%";
+    star.style.left = getRandomNumber(0, 100) + "%";
+    star.style.animationDelay = getRandomNumber(-10, 0) + "s"; // Adjusted the delay range to negative values
     star.style.animationDuration = getRandomNumber(5, 10) + "s";
-    star.style.setProperty('--parallax-direction', getRandomNumber(-4, 4) + 'px');
+    star.style.setProperty("--parallax-depth", getRandomNumber(1, 5));
+    star.style.setProperty("--parallax-direction-x", getRandomNumber(-10, 10) + "px");
+    star.style.setProperty("--parallax-direction-y", getRandomNumber(-10, 10) + "px");
+    starsContainer.appendChild(star);
   }
+}
+
+createStars();
