44feb45f9c828f74827080b5a8d54c71323ad158
Spelljinxer <reiden556@gmail.com>
2023-06-18 00:20:35 +0800

    some minor fixes

--- portfolio/home.js
+++ portfolio/home.js
@@ -1,52 +1,60 @@
-var cursor = document.querySelector('.cursor'),
-    cursorScale = document.querySelectorAll('.cursor-scale'),
-    section1 = document.querySelector('.section1'),
-    windowWidth = window.innerWidth,
-    windowHeight = window.innerHeight,
-    mouseX = windowWidth / 2,
-    mouseY = windowHeight / 2;
+var cursor = document.querySelector('.cursor');
+var cursorScale = document.querySelectorAll('.cursor-scale');
+var section1 = document.querySelector('.section1');
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+var mouseX = windowWidth / 2;
+var mouseY = windowHeight / 2;
 
 gsap.set(cursor, {
-    css: {
-        left: mouseX,
-        top: mouseY
-    }
+  css: {
+    left: mouseX,
+    top: mouseY
+  }
 });
 
 gsap.to({}, 0.016, {
-    repeat: -1,
-
-    onRepeat: function () {
-        gsap.set(cursor, {
-            css: {
-                left: mouseX,
-                top: mouseY
-            }
-        })
-    }
+  repeat: -1,
+  onRepeat: function() {
+    gsap.set(cursor, {
+      css: {
+        left: mouseX,
+        top: mouseY
+      }
+    });
+  }
 });
 
-window.addEventListener("mousemove", function (e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+window.addEventListener("mousemove", function(e) {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
 
-    if (!section1.contains(e.target)) {
-        cursor.style.display = 'none';
-    } else {
-        cursor.style.display = 'block';
-    }
+  if (!section1.contains(e.target)) {
+    cursor.style.display = 'none';
+  } else {
+    cursor.style.display = 'block';
+  }
 });
 
 cursorScale.forEach(link => {
-    link.addEventListener('mouseleave', () => {
-        cursor.classList.remove('grow');
-        cursor.classList.remove('grow-small');
-    });
-    link.addEventListener('mousemove', () => {
-        cursor.classList.add('grow');
-        if (link.classList.contains('small')) {
-            cursor.classList.remove('grow');
-            cursor.classList.add('grow-small');
-        }
-    });
+  link.addEventListener('mouseleave', () => {
+    cursor.classList.remove('grow');
+    cursor.classList.remove('grow-small');
+  });
+  link.addEventListener('mousemove', () => {
+    cursor.classList.add('grow');
+    if (link.classList.contains('small')) {
+      cursor.classList.remove('grow');
+      cursor.classList.add('grow-small');
+    }
+  });
+});
+
+// Trigger the mousemove event after page load
+window.addEventListener("load", function() {
+  var event = new MouseEvent("mousemove", {
+    clientX: windowWidth / 2,
+    clientY: windowHeight / 2
+  });
+  window.dispatchEvent(event);
 });
